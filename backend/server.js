48322b2f3d109f8196038cc64c75434b97490ed1
Bharani951 <bharani.22it@kct.ac.in>
2025-06-16 12:00:11 +0530

    all updated file

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,15 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 
 app.use("/api/todos", todoRoutes); // Use the todoRoutes for handling requests to /api/todos
 
+const __dirname = path.resolve(); // Get the current directory name
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist"))); // Serve static files from the frontend build directory in production
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")); // Serve the index.html file for any other routes
+  });
+}
+
 app.listen(5000, () => {
   connectDB(); // Call the function to connect to the database
   console.log("Server is running on the port http://localhost:5000");
