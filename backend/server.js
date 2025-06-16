86c1a33c1eaf4a22d699423335d1577c35fc01e1
Bharani951 <bharani.22it@kct.ac.in>
2025-06-16 12:35:37 +0530

    final commit-3

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,6 @@ import dotenv from "dotenv"; // to get access to mongo_uri
 import express from "express";
 import { connectDB } from "./config/db.js";
 import todoRoutes from "./routes/todo.routes.js"; // Import the todo routes for handling requests
-import path from "path";
-const PORT = process.env.PORT || 5000; // Set the port to the value from environment variables or default to 5000
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -17,16 +15,7 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 
 app.use("/api/todos", todoRoutes); // Use the todoRoutes for handling requests to /api/todos
 
-const __dirname = path.resolve(); // Get the current directory name
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist"))); // Serve static files from the frontend build directory in production
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")); // Serve the index.html file for any other routes
-  });
-}
-
-app.listen(PORT, () => {
+app.listen(5000, () => {
   connectDB(); // Call the function to connect to the database
   console.log("Server is running on the port http://localhost:5000");
 }); // listen and Start the server on port 5000
